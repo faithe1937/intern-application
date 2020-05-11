adf31b31425826453cf381a3acb7195eb253dc73
Faith <faithe1937@gmail.com>
2020-05-11 14:34:09 -0500

    random linear background generator

--- src/Child.js
+++ src/Child.js
@@ -1,11 +1,48 @@
 import React, { useState } from "react";
 import "./Child.css";
 
-function Child(props) {
+function Child({ color, setColor, setInterval, blink }) {
+  let [speed, setSpeed] = useState(1000);
+  let [colorTwo, setColorTwo] = useState("white");
+
+  const rando = () => {
+    const colors = ["Crimson", "Chocolate", "Coral", "LightSeaGreen"];
+    let newColor = colors[Math.floor(Math.random() * colors.length)];
+    setColor(newColor);
+    // color2.push(newColor);
+  };
+
+  function handleChange(e) {
+    setColorTwo(e.target.value);
+  }
+
+  const handleIncrement = () => {
+    setInterval(speed + 500);
+    setSpeed(speed + 500);
+  };
+
+  //Create handleDecrement event handler
+  const handleDecrement = () => {
+    setInterval(speed - 500);
+    setSpeed(speed - 500);
+  };
+  rando();
+
   return (
-    <div className='color-container'>
-      <div style={{ background: props.color }} className='color-box'>
-        box
+    <div
+      style={{ background: `linear-gradient(${color}, ${colorTwo})` }}
+      className='container'>
+      <div className='box'>
+        <div className='text'>
+          <h2>{color}</h2>
+          <h3>{speed}</h3>
+          <p>
+            <input value={colorTwo} onChange={handleChange} />
+            <br />
+            <button onClick={() => handleIncrement()}> slower </button>
+            <button onClick={() => handleDecrement()}> faster </button>
+          </p>
+        </div>
       </div>
     </div>
   );
